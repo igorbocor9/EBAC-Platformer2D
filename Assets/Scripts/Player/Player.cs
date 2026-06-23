27c4ed5d691ff6b799d80bbc72aa135f40488881
igorbocor9 <igor.bocor9@gmail.com>
2026-06-23 19:48:21 -0300

    added jump

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,19 +1,25 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 
 public class Player : MonoBehaviour
 { 
     public Rigidbody2D mRigidbody;
 
+    [Header("Speed Setup")]
     public Vector2 friction = new Vector2(.1f, 0);
-
     public float speed;
     public float speedRun;
-
     private float _currentSpeed;
-
     public float jumpForce = 2;
+
+    [Header("Animation Setup")]
+    public float jumpScaley= 1.5f;
+    public float jumpScalex = .7f;
+    public float animationDuration = 0.3f;
+    public Ease ease = Ease.OutBack;
+    public bool isJumping = false;
 
     private void Update()
     {
@@ -56,7 +62,18 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            isJumping = true;
             mRigidbody.linearVelocity = Vector2.up * jumpForce;
+            mRigidbody.transform.LocalScale = Vector2.one;
+            DOTween.Kill(mRigidbody.transform);
+            HandleScaleJump();
         }
     }
+
+    private void HandleScaleJump()
+    {
+        mRigidbody.transform.DOScaleY(jumpScaley, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        mRigidbody.transform.DOScaleX(jumpScalex, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+    }
+
 }
