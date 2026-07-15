0f175e50fe70cbe45ef0cea9b46598e983e8e30d
igorbocor9 <igor.bocor9@gmail.com>
2026-07-15 17:30:09 -0300

    update UI in classic mode

--- Assets/Scripts/Coin/ItemManager.cs
+++ Assets/Scripts/Coin/ItemManager.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 using Core.Singleton;
+using TMPro;
 
 public class ItemManager : Singleton<ItemManager>
 {
     public int coins;
+    public TextMeshProUGUI uiTextCoins;
 
     private void Start()
     {
@@ -13,10 +15,17 @@ public class ItemManager : Singleton<ItemManager>
     private void Reset()
     {
         coins = 0;
+        UpdateUI();
     }
 
     public void AddCoins(int amount = 1)
     {
         coins += amount;
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        uiTextCoins.text = coins.ToString();
     }
 }
