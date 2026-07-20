0778e79dc3c1c86d61e2e818b019c8fa28819b75
igorbocor9 <igor.bocor9@gmail.com>
2026-07-20 18:02:21 -0300

    adding VFX to the coins

--- Assets/Scripts/Coin/ItemCollectableBase.cs
+++ Assets/Scripts/Coin/ItemCollectableBase.cs
@@ -3,6 +3,17 @@ using UnityEngine;
 public class ItemCollectableBase : MonoBehaviour
 {
     public string compareTag = "Player";
+    public ParticleSystem particleSystem;
+    public float timeToHide = 3f;
+    public GameObject graphicItem;
+
+    private void Awake()
+    {
+        if (particleSystem != null)
+        {
+            //particleSystem.transform.SetParent(null);
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -14,11 +25,24 @@ public class ItemCollectableBase : MonoBehaviour
 
     protected virtual void Collect()
     {
-        gameObject.SetActive(false);
+        if (graphicItem != null)
+        {
+            graphicItem.SetActive(false);
+        }
+        Invoke("HideObject", timeToHide);
         OnCollect();
     }
 
     protected virtual void OnCollect()
     {
+        if (particleSystem != null)
+        {
+            particleSystem.Play();
+        }
+    }
+
+    private void HideObject()
+    {
+        gameObject.SetActive(false);
     }
 }
